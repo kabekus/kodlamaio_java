169430b4558feddcf89f45dc49c24e6a54d187ae
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-09-20 22:41:54 +0300

    ReCap Demo

---
+++ java/src/ReCapDemo.java
@@ -0,0 +1,15 @@
+public class ReCapDemo {
+    public static void main(String[] args) {
+
+        int num1=20, num2=25, num3=2, maxNum=num1;
+
+        if (maxNum<num2){
+            maxNum = num2;
+        }else if (maxNum<num3){
+            maxNum = num3;
+        }
+
+        System.out.print("Largest number: "+maxNum);
+    }
+}
+
