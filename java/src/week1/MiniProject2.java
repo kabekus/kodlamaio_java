f22ea58cd1a2da72707d6c8da57e95991a682d68
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-09-21 19:49:20 +0300

    Mini Project 2 "Bass And Thin Vowels"

---
+++ java/src/week1/MiniProject2.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class MiniProject2 {
+    public static void main(String[] args) {
+        char letter = 'A';
+        switch (letter){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+              System.out.println("Kalın sesli harf");
+              break;
+            default:
+                System.out.println("İnce sesli harf");
+        }
+    }
+}
