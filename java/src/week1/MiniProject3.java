3aa8c915c9853c4ddcd795211ce5c06b29702265
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-09-21 19:56:42 +0300

    Mini Project 3 "Perfect Numbers"

---
+++ java/src/week1/MiniProject3.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class MiniProject3 {
+    public static void main(String[] args) {
+        int num = 5, total = 0;
+
+        for (int i=1 ; i<num ; i++){
+            if (num % 1 == 0){
+                total += i ;
+            }
+        }
+
+        if (total == num){
+            System.out.println("Mükemmel sayıdır");
+        }else
+            System.out.println("Mükemmel sayıdır");
+    }
+}
